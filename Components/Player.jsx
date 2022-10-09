f99682a29528097ad99befa4169ee859959d23ae
Chibuzor <chibuzorm26@example.com>
2022-10-09 21:30:38 +0100

    god help me

--- Components/Player.jsx
+++ Components/Player.jsx
@@ -84,25 +84,25 @@ function Player() {
       </div>
 
       <div className="flex items-center justify-evenly">
-        <SwitchHorizontalIcon className="button" />
+        <SwitchHorizontalIcon className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
         <RewindIcon
-          /*  onClick={()=>SpotifyApi.skipToPrevious()} */ className="button"
+          /*  onClick={()=>SpotifyApi.skipToPrevious()} */ className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
         />
         {isPlaying ? (
-          <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+          <PauseIcon onClick={handlePlayPause} className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
         ) : (
-          <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
+          <PlayIcon onClick={handlePlayPause} className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
         )}
 
-        <FastForwardIcon className="button" />
-        <ReplyIcon className="button" />
+        <FastForwardIcon className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
+        <ReplyIcon className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
       </div>
       <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
         <VolumeOffIcon
           onClick={() => {
             volume > 0 && setVolume(volume - 10);
           }}
-          className="button"
+          className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
         />
         <input
           className="w-14 md:w-28"
@@ -116,7 +116,7 @@ function Player() {
           onClick={() => {
             volume < 100 && setVolume(volume + 10);
           }}
-          className="button"
+          className="h-5 w-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
         />
       </div>
     </div>
