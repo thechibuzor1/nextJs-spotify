3bc43fb5da73d67bc552e0597ccd2fc609fa9cdb
Chibuzor <chibuzorm26@example.com>
2022-10-09 19:54:31 +0100

    jesus christ another fix :)

--- middleware.tsx
+++ middleware.tsx
@@ -1,11 +1,13 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(req) {
-
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
   if (req.nextUrl.pathname === "/") {
     const session = await getToken({ req, secret: process.env.JWT_SECRET });
-    if (!session) return NextResponse.redirect("/Login");
+    url.pathname = "/Login";
+    if (!session) return NextResponse.redirect(url);
   }
   return NextResponse.next();
 }
