a384195d9901a12c3dd979f4da311737f1dc97bc
Chibuzor <chibuzorm26@example.com>
2022-10-09 20:05:22 +0100

    fix

--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,14 @@ export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   if (req.nextUrl.pathname === "/") {
     const session = await getToken({ req, secret: process.env.JWT_SECRET });
-    url.pathname = "/Login";
-    if (!session) return NextResponse.redirect(url);
+    if (!session) {
+      url.pathname = "/Login";
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
   }
-  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/", "/Login"],
+};
